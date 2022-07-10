321cb220632a6b3f58af643d25c759ef95917416
Mohsen Goodarzi <mgdana@outlook.com>
2022-07-10 20:01:49 +0100

    initialised

---
+++ FoodLabellingSystem_Service/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodLabellingSystem_Service.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
